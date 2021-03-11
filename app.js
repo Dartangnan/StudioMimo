5b6a0dd4417ea32e59d6163c454b55200eca9590
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-10 17:27:07 -0800

    Second commit with node

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.listen(process.enc.PORT || 3000, function () {
+  console.log("Server runnin on port 3000.");
+});
