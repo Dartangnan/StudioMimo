a373eac2f808be897532d014ebc6636ee6383d19
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-11 12:39:32 -0800

    15 commit - links working and nav bar sticking to top

--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
 // ============== Declaration of variables ==============
 
+const body = document.querySelector("body");
 const mainTitle = document.querySelector(".main-text-home");
 const mainLinkHome = document.querySelector(".link-home");
 const navBarIcon = document.querySelector(".nav-ham-icon");
-const navBarMobile = document.querySelector(".nav-items-m");
+const itemsNavBarMobile = document.querySelector(".nav-items.mobile");
+const navItems = document.querySelectorAll(".nav-items");
+const bottomNavItems = document.querySelectorAll(".bottom-nav-items a");
+const navBar = document.querySelector(".nav-bar");
 
 // ============== Declaration of functions ==============
-
+const rollSmoothTo = function (sectionName) {
+  const sectionTo = document.querySelector(`#${sectionName.replace(" ", "-")}`);
+  setTimeout(
+    function () {
+      sectionTo.scrollIntoView({ behavior: "smooth" });
+    },
+    150,
+    sectionTo
+  );
+};
 // ===================== Scripts =======================
 
 // Loading animation in the home page:
@@ -20,19 +33,55 @@ setTimeout(function () {
 }, 1000);
 
 // Nav Bar movement for mobile:
-document.querySelector("body").addEventListener("click", function (e) {
-  console.log(e.target.classList.value);
-  console.log(e.target.classList.value === "fas fa-bars");
-  console.log(e.target.classList.value === "nav-ham-icon");
+body.addEventListener("click", function (e) {
   if (
     e.target.classList.value === "fas fa-bars" ||
     e.target.classList.value === "nav-ham-icon"
   ) {
-    console.log("in");
-    navBarMobile.classList.toggle("nav-items-hidden");
+    itemsNavBarMobile.classList.toggle("nav-items-hidden");
   } else {
-    if (!navBarMobile.classList.contains("nav-items-hidden")) {
-      navBarMobile.classList.add("nav-items-hidden");
+    if (!itemsNavBarMobile.classList.contains("nav-items-hidden")) {
+      itemsNavBarMobile.classList.add("nav-items-hidden");
     }
   }
 });
+
+// Nav Bar links to the different sections of the website:
+navItems.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    rollSmoothTo(e.target.outerText);
+  });
+});
+
+bottomNavItems.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    rollSmoothTo(e.target.outerText);
+  });
+});
+
+mainLinkHome.addEventListener("click", function (e) {
+  rollSmoothTo("Contato");
+});
+
+const oldPosMin = 0;
+let oldPos = 70;
+window.onscroll = function () {
+  if (!itemsNavBarMobile.classList.contains("nav-items-hidden")) {
+    itemsNavBarMobile.classList.add("nav-items-hidden");
+  }
+  if (pageYOffset > oldPosMin && pageYOffset < oldPos) {
+    navBar.style.position = "fixed";
+    navBar.style.width = "100%";
+    navBar.style.transform = "translatey(0)";
+    oldPos = pageYOffset;
+  } else if (pageYOffset > oldPosMin && pageYOffset > oldPos) {
+    navBar.style.position = "fixed";
+    navBar.style.width = "100%";
+    oldPos = pageYOffset;
+    navBar.style.transform = "translatey(-100%)";
+  } else {
+    navBar.style.transform = "translatey(0)";
+    navBar.style.position = "absolute";
+  }
+  console.log(pageYOffset);
+};
