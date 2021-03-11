799eabfb192c92580fadf0c4fe6dfb2bf547e85c
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-11 10:25:55 -0800

    15 commit, change in the contact top img and js organization

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
+// ============== Declaration of variables ==============
+
 const mainTitle = document.querySelector(".main-text-home");
 const mainLinkHome = document.querySelector(".link-home");
 const navBarIcon = document.querySelector(".nav-ham-icon");
 const navBarMobile = document.querySelector(".nav-items-m");
 
+// ============== Declaration of functions ==============
+
+// ===================== Scripts =======================
+
+// Loading animation in the home page:
 setTimeout(function () {
   {
     mainTitle.classList.remove("main-text-hidden");
@@ -12,6 +19,7 @@ setTimeout(function () {
   }
 }, 1000);
 
+// Nav Bar movement for mobile:
 document.querySelector("body").addEventListener("click", function (e) {
   console.log(e.target.classList.value);
   console.log(e.target.classList.value === "fas fa-bars");
