510c7c721a6df201e87168c0c0433fb93e858c0c
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-11 09:06:17 -0800

    13 commit - Nav items now toggle between hidden and showing

--- public/script.js
+++ public/script.js
@@ -1,14 +1,30 @@
-/*document.querySelector(".home").style.height = `${
-  document.querySelector(".bottom-img").height
-}px`;*/
+const mainTitle = document.querySelector(".main-text-home");
+const mainLinkHome = document.querySelector(".link-home");
+const navBarIcon = document.querySelector(".nav-ham-icon");
+const navBarMobile = document.querySelector(".nav-items-m");
 
 setTimeout(function () {
   {
-    document
-      .querySelector(".main-text-home")
-      .classList.remove("main-text-hidden");
+    mainTitle.classList.remove("main-text-hidden");
     setTimeout(function () {
-      document.querySelector(".link-home").classList.remove("link-hidden");
+      mainLinkHome.classList.remove("link-hidden");
     }, 1000);
   }
 }, 1000);
+
+document.querySelector("body").addEventListener("click", function (e) {
+  console.log(e.target.classList.value);
+  console.log(e.target.classList.value === "fas fa-bars");
+  console.log(e.target.classList.value === "nav-ham-icon");
+  if (
+    e.target.classList.value === "fas fa-bars" ||
+    e.target.classList.value === "nav-ham-icon"
+  ) {
+    console.log("in");
+    navBarMobile.classList.toggle("nav-items-hidden");
+  } else {
+    if (!navBarMobile.classList.contains("nav-items-hidden")) {
+      navBarMobile.classList.add("nav-items-hidden");
+    }
+  }
+});
