3f41ca8d3c9675eb4485b907c5e1b996f6359979
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-03-12 14:16:58 -0800

    29 commit - lazy loading v1

--- public/script.js
+++ public/script.js
@@ -9,6 +9,7 @@ const navItems = document.querySelectorAll(".nav-items");
 const bottomNavItems = document.querySelectorAll(".bottom-nav-items a");
 const navBar = document.querySelector(".nav-bar");
 const footerLinks = document.querySelector(".icons");
+const sectionsAll = document.querySelectorAll(".section");
 
 // ============== Declaration of functions ==============
 const rollSmoothTo = function (sectionName) {
@@ -21,9 +22,24 @@ const rollSmoothTo = function (sectionName) {
     sectionTo
   );
 };
+
+const createSectionObserver = function () {
+  const lazyLoading = function (entries, observer) {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("section-hidden");
+      observer.unobserve(entry.target);
+    }
+  };
+  return new IntersectionObserver(lazyLoading, {
+    root: null,
+    thershold: 0.15,
+  });
+};
+
 // ===================== Scripts =======================
 
-// Loading animation in the home page:
+// --- Loading animation in the home page: ---
 setTimeout(function () {
   {
     mainTitle.classList.remove("main-text-hidden");
@@ -33,7 +49,7 @@ setTimeout(function () {
   }
 }, 1000);
 
-// Nav Bar movement for mobile:
+// --- Nav Bar movement for mobile: ---
 body.addEventListener("click", function (e) {
   if (
     e.target.classList.value === "fas fa-bars" ||
@@ -47,7 +63,7 @@ body.addEventListener("click", function (e) {
   }
 });
 
-// Links to the different sections of the website:
+// --- Links to the different sections of the website: ---
 // Nav bar links
 navItems.forEach((el) => {
   el.addEventListener("click", function (e) {
@@ -67,11 +83,12 @@ mainLinkHome.addEventListener("click", function (e) {
   rollSmoothTo("Contato");
 });
 
-// Make sure that the nav bar shows up if scrolling up and desapears if scrolling down
+// --- Make sure that the nav bar shows up if scrolling up and desapears if scrolling down ---
 const oldPosMin = 0;
 let oldPos = 70;
 let altura = 0;
 window.onscroll = function () {
+  // Nav items are hidden if scrolling:
   if (!itemsNavBarMobile.classList.contains("nav-items-hidden")) {
     itemsNavBarMobile.classList.add("nav-items-hidden");
   }
@@ -89,3 +106,16 @@ window.onscroll = function () {
     navBar.style.transform = "translatey(0)";
   }
 };
+
+// --- Lazy loading of the pages ---
+/*
+const obsOpition = {
+  root: null, //object that the target is intersecting
+  threshold: 0.1, //percentage of intersection between the target element and the root element
+};*/
+
+// Add hidden class and create a observer for each section:
+sectionsAll.forEach((section) => {
+  section.classList.add("section-hidden");
+  createSectionObserver().observe(section);
+});
